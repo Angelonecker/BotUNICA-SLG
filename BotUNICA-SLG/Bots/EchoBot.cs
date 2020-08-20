6c873e2cb64b45d9025fd71d446a30261e26785f
Angelonecker <69283162+Angelonecker@users.noreply.github.com>
2020-08-20 11:52:37 -0500

    Update EchoBot.cs

--- BotUNICA-SLG/Bots/EchoBot.cs
+++ BotUNICA-SLG/Bots/EchoBot.cs
@@ -13,18 +13,30 @@ namespace Microsoft.BotBuilderSamples.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
+<<<<<<< HEAD
             var replyText = $"BotUNICA-SLG: {turnContext.Activity.Text}";
+=======
+            var replyText = $"Echo: {turnContext.Activity.Text}";
+>>>>>>> parent of 2e3fe8b... Revert "Create BotUNICA-SLG-src.zip"
             await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
+<<<<<<< HEAD
             var welcomeText = "Hola Innovador!";
+=======
+            var welcomeText = "Hello and welcome!";
+>>>>>>> parent of 2e3fe8b... Revert "Create BotUNICA-SLG-src.zip"
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
+<<<<<<< HEAD
                     await turnContext.SendActivityAsync(MessageFactory.Text($"Hola Innovador!"), cancellationToken);
+=======
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+>>>>>>> parent of 2e3fe8b... Revert "Create BotUNICA-SLG-src.zip"
                 }
             }
         }
