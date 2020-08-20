db09ce9b6aa65aa92157ebe54f6fc740d811907a
Angelonecker <69283162+Angelonecker@users.noreply.github.com>
2020-08-20 11:51:40 -0500

    Revert "ve224"
    This reverts commit 5f2c05d49e2acbf6751d72fd93aaa6c2cb2e88af.

--- BotUNICA-SLG/Bots/EchoBot.cs
+++ BotUNICA-SLG/Bots/EchoBot.cs
@@ -13,13 +13,13 @@ namespace Microsoft.BotBuilderSamples.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"BotUNICA: {turnContext.Activity.Text}";
+            var replyText = $"BotUNICA-SLG: {turnContext.Activity.Text}";
             await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hola Bienvenido";
+            var welcomeText = "Hola Angelo!";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
